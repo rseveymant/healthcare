56998c74ba730877dd1f84d698694d0cf03a7b4c
rseveymant <123193740+rseveymant@users.noreply.github.com>
2023-08-03 21:20:22 +0000

    tiny change

--- app/utils/code_mapper.py
+++ app/utils/code_mapper.py
@@ -13,12 +13,13 @@ class ICD10Generator:
         self.gpt_model = gpt_model
 
     def generate(self, input_text):
+        print(input_text)
         # Step 1: Use Mantium's RAG model to generate ICD-10 candidates
         rag_candidates = self.apps_api.query_application('64cbf1271dbc4547923267cd', dict(query=input_text, retriever_top_k=1))
 
         # Step 2: Refine candidates using GPT model
         print(rag_candidates['documents'][0]['content'])
-        refinement_prompt = f"Consider the original summary: {input_text} and the top result {rag_candidates['documents'][0]['content']} and return the correct ICD-10 code"
+        refinement_prompt = f"Consider the original summary: {input_text} and the top result {rag_candidates['documents'][0]['content']} and return ALL relevant and correct ICD-10 code"
         
         #Step 2: Refine candidates using GPT model
         refined_answer = self.gpt_model.generate(refinement_prompt)
