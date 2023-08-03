dc9571ea94f6112048cba61496a894238505f455
rseveymant <123193740+rseveymant@users.noreply.github.com>
2023-08-03 16:49:27 +0000

    adding prior auth

--- app/routes.py
+++ app/routes.py
@@ -11,3 +11,26 @@ def generate_text():
         prompt = request.form.get('prompt', '')
         response = gpt_service.generate(prompt)
     return render_template('index.html', response=response)
+
+@app.route('/prior_auth', methods=['GET', 'POST'])
+def prior_auth():
+    if request.method == 'POST':
+        patient_info = request.form.get('patient_info', '')
+        treatment_plan = request.form.get('treatment_plan', '')
+        
+        # Combine patient_info and treatment_plan into a single prompt.
+        # Modify this according to your needs
+        prompt = f"Patient information: {patient_info}\nTreatment Plan: {treatment_plan}"
+        
+        # Generate prior authorization document text
+        document_text = gpt_service.generate(prompt)
+        
+        # Convert text to a file
+        mem = io.StringIO()
+        mem.write(document_text)
+        mem.seek(0)
+        
+        # Send file
+        return send_file(mem, as_attachment=True, attachment_filename='prior_authorization.txt', mimetype='text/plain')
+
+    return render_template('prior_auth_form.html')
